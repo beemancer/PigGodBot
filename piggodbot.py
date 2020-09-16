2f5f71b7e6b498b1cd9c43854618e16078d8b503
beemancer <59190135+beemancer@users.noreply.github.com>
2020-09-15 19:32:22 -0700

    Update piggodbot.py

--- piggodbot.py
+++ piggodbot.py
@@ -40,7 +40,7 @@ async def SeupFFXIV():
     xivClient = xivapi.Client(session=session, api_key=xivkey)
     xivClientReady = True
 
-eqCalendarId = 'nujrnhog654g3v0m0ljmjbp790@group.calendar.google.com'
+eqCalendarId = 'pso2.schedule@gmail.com'
 eqChannels = {}
 try:
     with open('eqchannels', 'r') as eqChannelsFile:
